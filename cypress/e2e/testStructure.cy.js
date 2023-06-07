bc7a2e5a69f6954c1c7568fd124ef4900bee8107
betululutin <betululutin@hotmail.com>
2023-06-07 13:39:10 +0200

    add: first commit

---
+++ cypress/e2e/testStructure.cy.js
@@ -0,0 +1,26 @@
+/// <reference types="cypress" />
+
+describe('Context: My First Tests',()=>{
+    before(()=>{
+        // runs once before all test cases in this describe block, beforeCLass in TestNG
+    })
+
+    beforeEach(() => {
+        //run before each test case, beforeMethod in TestNG
+        cy.clearCookies();
+    })
+
+    after(() => {
+        // similar to afterClass in TestNG, runs once after all tests finished
+    })
+
+    afterEach(() => {
+        // similar to afterMethod in TestNG
+    })
+
+    it('Opening a web application,',() => {
+        cy.visit('/registration_form');    
+    })
+
+
+})
