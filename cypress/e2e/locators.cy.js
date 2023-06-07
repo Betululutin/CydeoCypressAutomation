081b3c350217ec769b4df8e87ff43b762235fddb
betululutin <betululutin@hotmail.com>
2023-06-07 15:35:20 +0200

    add: more tests

---
+++ cypress/e2e/locators.cy.js
@@ -0,0 +1,36 @@
+/// <reference types="cypress" />
+
+describe('Find or Get Elements by using Different Locators ',()=>{
+    
+
+    beforeEach(() => {
+        //run before each test case, beforeMethod in TestNG
+        cy.clearCookies();
+        cy.visit('/login');
+    })
+
+    
+
+    it('Check different locators strategies',() => {
+          // by CSS locator
+          cy.get("input[name='username']").type("CydeoStudent");
+          //every statement creates an object to be interacted, and next command makes operation to the object created
+          //at the previous statement
+
+          //attribute name and value
+          cy.get("[type='text']").clear(); //clear what is typed
+
+          cy.get("input").each((item,index,list) => {
+            //assert the lenght of the list is 2
+            expect(list).to.have.length(2);
+            expect(item).to.have.attr("type");
+
+          })
+
+    
+          
+    })
+
+    
+
+})
