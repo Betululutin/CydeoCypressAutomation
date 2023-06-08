55c2e55a2b2d0efd58031d27df454f4cd4d1a613
betululutin <betululutin@hotmail.com>
2023-06-08 12:48:21 +0200

    add:assertions

--- cypress/e2e/buttons.cy.js
+++ cypress/e2e/buttons.cy.js
@@ -15,6 +15,27 @@ describe('Context: My First Tests ',()=>{
           // select a button with text
           cy.contains('Button 2').should('be.visible').click();
           cy.contains('Clicked on button two!').should('be.visible');
+
+
+        // find element with class attribute and create a list then select 3rd element frim the list
+
+     cy.get('.btn.btn-primary').then(($buttons) => {
+     cy.wrap($buttons).eq(2).click();
+
+      //assert the text
+     cy.contains('Clicked on button three!').should('be.visible');
+
+   
+
+     })
+
+
+
+
     })
+
+
+   
+
 
 })
